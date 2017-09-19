ba31c093d1d92e4b08e521d5b2ea12650fc64722
Modesto <luis.augusto.ramos@gmail.com>
2017-09-19 15:58:36 -0300

    CameraTest2

--- QA the Game/Assets/_Scripts/CameraFollow.cs
+++ QA the Game/Assets/_Scripts/CameraFollow.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollow : MonoBehaviour {
+	public float dampTime = 0.15f;
+	private Vector3 velocity = Vector3.zero;
+	public Transform player;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update () 
+	{
+		if (player)
+		{
+			Vector3 point = camera.WorldToViewportPoint(player.position);
+			Vector3 delta = player.position - camera.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, point.z)); //(new Vector3(0.5, 0.5, point.z));
+			Vector3 destination = transform.position + delta;
+			transform.position = Vector3.SmoothDamp(transform.position, destination, ref velocity, dampTime);
+		}
+
 	}
 }

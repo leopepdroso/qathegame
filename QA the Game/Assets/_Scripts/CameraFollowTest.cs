a698db5eb90e062b4d84694aea866e38fce6194e
Modesto <luis.augusto.ramos@gmail.com>
2017-09-20 10:55:03 -0300

    CameraFollowTest2

--- QA the Game/Assets/_Scripts/CameraFollowTest.cs
+++ QA the Game/Assets/_Scripts/CameraFollowTest.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraFollowTest : MonoBehaviour {
+	[SerializeField]
+	private float Xmin;
+	[SerializeField]
+	private float Ymin;
+	[SerializeField]
+	private float Xmax;
+	[SerializeField]
+	private float Ymax;
+
+
+
 	public GameObject player; 
-	private Vector3 offset; 
+
+	//private Vector3 offset; 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;
+		//offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = player.transform.position + offset;
+		transform.position = new Vector3 (Mathf.Clamp (player.transform.position.x, Xmin, Xmax), Mathf.Clamp (player.transform.position.y, Ymin, Ymax), transform.position.z);
 	}
 }

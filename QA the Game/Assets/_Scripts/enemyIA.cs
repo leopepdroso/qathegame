ac2199be958879cfbeaf506f591ec0fe57c328b5
Thiago Mohr <thiagomohrs@gmail.com>
2017-09-22 10:30:17 -0300

    IA

--- QA the Game/Assets/_Scripts/enemyIA.cs
+++ QA the Game/Assets/_Scripts/enemyIA.cs
@@ -8,21 +8,17 @@ public class enemyIA : MonoBehaviour {
     private GameObject Enemy;
     private GameObject Player;
     private float Range;
-    public float Speed;
 
-
-    // Use this for initialization
     void Start()
     {
         Enemy = GameObject.FindGameObjectWithTag("Enemy");
         Player = GameObject.FindGameObjectWithTag("Player");
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         Range = Vector2.Distance(Enemy.transform.position, Player.transform.position);
-        if (Range <= 15f)
+        if (Range <=2f)
         {
             Debug.Log("Enemy nearby");
         }
